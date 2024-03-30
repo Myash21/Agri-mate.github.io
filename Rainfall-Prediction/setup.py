f2140ece15c9142c3005a20a96a35a4f1f6975be
Yash Mhaskar <myash100@gmail.com>
2024-03-30 03:34:24 -0700

    commit 1

---
+++ Rainfall-Prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end to end application to predict rainfall in India',
+    author='Yash Mhaskar',
+    license='MIT',
+)
